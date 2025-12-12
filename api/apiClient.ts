380a9bb491548cbbb24b0b943f13d6930ce8e497
sem1hs <semihgtt11@gmail.com>
2025-12-12 20:42:08 +0300

    dependcyler eklendi

---
+++ api/apiClient.ts
@@ -0,0 +1,51 @@
+import "server-only";
+import axios, {
+  AxiosError,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+
+export const apiClient = axios.create({
+  baseURL: process.env.API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+const logOnDev = (
+  message: string,
+  log?: AxiosResponse | InternalAxiosRequestConfig | AxiosError
+) => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(message, log);
+  }
+};
+
+apiClient.interceptors.response.use(
+  (response) => {
+    const { method, url } = response.config;
+    const { status } = response;
+
+    logOnDev(
+      `✨ [${method?.toUpperCase()}] ${url} | Response ${status}`,
+      response
+    );
+
+    return response;
+  },
+  (error) => {
+    const { message } = error;
+    const { status, data } = error.response;
+    const { method, url } = error.config;
+
+    logOnDev(
+      `🚨 [${method?.toUpperCase()}] ${url} | Error ${status} ${
+        data?.message || ""
+      } | ${message}`,
+      error
+    );
+
+    return Promise.reject(error);
+  }
+);
