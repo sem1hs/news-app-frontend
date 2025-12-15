886d8dd18ca26c4975ea0667c2aaf7934ea1871c
sem1hs <semihgtt11@gmail.com>
2025-12-15 22:52:16 +0300

    delay eklendi

--- components/Header/Dropdown.tsx
+++ components/Header/Dropdown.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Dropdown = ({ dropdownItems }: Props) => {
   return (
     <div className="absolute top-full left-1/2 -translate-x-1/2 pt-0.5 pointer-events-none">
-      <div className="w-48 rounded-xl bg-[#111517] shadow-lg border border-zinc-800 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition pointer-events-auto">
+      <div className="w-48 rounded-xl bg-[#111517] shadow-lg border border-zinc-800 opacity-0 invisible group-hover:opacity-100 group-hover:visible transition delay-100 pointer-events-auto">
         <ul className="flex flex-col py-3">
           {dropdownItems?.map((dropdownItem, i) => (
             <li
