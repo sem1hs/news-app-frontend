7908fd27d932cf78e5c1dbdfa936d69c187db6d2
sem1hs <semihgtt11@gmail.com>
2026-05-08 13:27:49 +0300

    populer news süresi uzatıldı

--- hooks/usePopularNews.ts
+++ hooks/usePopularNews.ts
@@ -1,22 +1,22 @@
 import { fetchPopularNews } from "@/api/news/news";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 
-const DAY_RANGE: number = 7;
+const DAY_RANGE: number = 60;
 
 const usePopularNews = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const getPopular = useQuery({
-        queryKey: ["news", "popular"],
-        queryFn: () => fetchPopularNews(DAY_RANGE),
-        staleTime: 1000 * 60 * 10,
-    });
+  const getPopular = useQuery({
+    queryKey: ["news", "popular"],
+    queryFn: () => fetchPopularNews(DAY_RANGE),
+    staleTime: 1000 * 60 * 10,
+  });
 
-    return {
-        popularNews: getPopular.data,
-        isLoading: getPopular.isLoading,
-        isError: getPopular.isError
-    };
-}
+  return {
+    popularNews: getPopular.data,
+    isLoading: getPopular.isLoading,
+    isError: getPopular.isError,
+  };
+};
 
-export default usePopularNews
+export default usePopularNews;
